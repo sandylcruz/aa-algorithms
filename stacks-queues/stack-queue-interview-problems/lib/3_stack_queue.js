0107ac6e8305139f681a86869c644a83bd72d867
Sandy Cruz <sandyleahcruz@icloud.com>
2021-06-09 19:31:04 -0700

    Implement pop on stack

--- stacks-queues/stack-queue-interview-problems/lib/3_stack_queue.js
+++ stacks-queues/stack-queue-interview-problems/lib/3_stack_queue.js
@@ -8,8 +8,8 @@
 //
 // Implement your preferred Stack implementation, including the methods:
 //
-//   - Push 
-//   - Pop 
+//   - Push
+//   - Pop
 //   - Size
 //
 // Then, implement a Queue by instantiating two Stack instances for storage.
@@ -25,19 +25,61 @@
 // -----------
 
 class Node {
-    // TODO: Implement the Node class!
-
+  // TODO: Implement the Node class!
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
 }
 
 class Stack {
-    // TODO: Implement the Stack class!
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
 
+  push(value) {
+    const newNode = new Node(value.value);
+
+    if (this.length === 0) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      const formerTopItem = this.top;
+      this.top = newNode;
+      this.top.next = formerTopItem;
+    }
+    this.length++;
+    return this.length;
+  }
+
+  pop() {
+    const itemToRemove = this.top;
+
+    if (this.length === 0) {
+      return null;
+    } else if (this.length === 1) {
+      this.bottom = null;
+      this.top = null;
+    } else {
+      this.top = this.top.next;
+    }
+
+    this.length--;
+    console.log("****", itemToRemove.value);
+    return itemToRemove.value;
+  }
+
+  size() {
+    return this.length;
+  }
+  // TODO: Implement the Stack class!
 }
 
 class StackQueue {
-    // TODO: Implement the StackQueue class!
-
-};
+  // TODO: Implement the StackQueue class!
+}
 
 exports.Node = Node;
 exports.Stack = Stack;
