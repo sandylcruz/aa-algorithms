61c76eb3bde3d9e0121447d33a50c66b76b8689e
Sandy Cruz <sandyleahcruz@icloud.com>
2021-06-09 18:22:40 -0700

    Create node, queue, and write enqueue function

--- stacks-queues/queue-project/lib/queue.js
+++ stacks-queues/queue-project/lib/queue.js
@@ -20,9 +20,51 @@
 // Let's Code!
 // -----------
 
-class Node {}
+class Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
 
-class Queue {}
+class Queue {
+  constructor(front, back) {
+    this.front = front;
+    this.back = back;
+    this.length = 0;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+
+    if (!this.front) {
+      this.front = newNode;
+      this.back = newNode;
+    } else {
+      this.back.next = newNode;
+      this.back = newNode;
+    }
+
+    this.length += 1;
+    return this.length;
+  }
+
+  dequeue() {
+    if (!this.front) {
+      return null;
+    } else if (this.length === 1) {
+      this.front = null;
+      this.back = null;
+    } else if (this.length > 1) {
+    }
+
+    --this.length;
+  }
+
+  size() {
+    return this.length;
+  }
+}
 
 exports.Node = Node;
 exports.Queue = Queue;
