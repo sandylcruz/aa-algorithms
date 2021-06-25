344e531928ffec150742dc0d8e40e3b47b6738a2
Sandy Cruz <sandyleahcruz@icloud.com>
2021-06-25 13:44:53 -0700

    Write .insert for binary search tree

--- trees-and-bst/bst_project/lib/bst.js
+++ trees-and-bst/bst_project/lib/bst.js
@@ -1,17 +1,41 @@
 class TreeNode {
-    constructor(val) {
-        this.val = val;
-        this.left = null;
-        this.right = null;
-    }
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
 }
 
-
 class BST {
-   
+  constructor() {
+    this.root = null;
+  }
+
+  insert(val, root = this.root) {
+    const newNode = new TreeNode(val);
+
+    if (!this.root) {
+      this.root = newNode;
+      return;
+    }
+
+    if (val < root.val) {
+      if (!root.left) {
+        root.left = newNode;
+      } else {
+        this.insert(val, root.left);
+      }
+    } else {
+      if (!root.right) {
+        root.right = newNode;
+      } else {
+        this.insert(val, root.right);
+      }
+    }
+  }
 }
 
 module.exports = {
-    TreeNode,
-    BST
+  TreeNode,
+  BST,
 };
