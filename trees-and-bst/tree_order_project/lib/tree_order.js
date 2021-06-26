82a954b71c786c1d462c36442cb6b2820d49b5e9
Sandy Cruz <sandyleahcruz@icloud.com>
2021-06-26 11:57:34 -0700

    Return in-order and post-order traversals

--- trees-and-bst/tree_order_project/lib/tree_order.js
+++ trees-and-bst/tree_order_project/lib/tree_order.js
@@ -1,13 +1,20 @@
 function inOrderArray(root) {
+  if (root === null) return [];
 
+  return [...inOrderArray(root.left), root.val, ...inOrderArray(root.right)];
 }
 
 function postOrderArray(root) {
+  if (root === null) return [];
 
+  return [
+    ...postOrderArray(root.left),
+    ...postOrderArray(root.right),
+    root.val,
+  ];
 }
 
-
 module.exports = {
-    inOrderArray,
-    postOrderArray
+  inOrderArray,
+  postOrderArray,
 };
