3533a9d1fb63342693995fb9c1011c9613b480ad
Sandy Cruz <sandyleahcruz@icloud.com>
2021-06-27 11:53:42 -0700

    Return tree height

--- trees-and-bst/depth_breadth_first_project/lib/tree_height.js
+++ trees-and-bst/depth_breadth_first_project/lib/tree_height.js
@@ -1,8 +1,27 @@
 function treeHeight(root) {
+  let numberOfEdges = 0;
+  if (!root) return -1;
 
+  let stack = [root];
+
+  while (stack.length) {
+    let node = stack.pop();
+    if (node.left && !node.right) {
+      numberOfEdges += 1;
+      stack.push(node.left);
+    } else if (node.right && !node.left) {
+      numberOfEdges += 1;
+      stack.push(node.right);
+    } else if (node.right && node.left) {
+      numberOfEdges += 1;
+      stack.push(node.left);
+      stack.push(node.right);
+    }
+  }
+
+  return numberOfEdges;
 }
 
-
 module.exports = {
-    treeHeight
+  treeHeight,
 };
