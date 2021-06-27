f5aa603293fd60dbe46b4cde3330839938007d34
Sandy Cruz <sandyleahcruz@icloud.com>
2021-06-27 11:24:35 -0700

    Write bfs

--- trees-and-bst/depth_breadth_first_project/lib/breadth_first_array.js
+++ trees-and-bst/depth_breadth_first_project/lib/breadth_first_array.js
@@ -1,7 +1,23 @@
 function breadthFirstArray(root) {
+  const queue = [root];
+  const visited = [];
 
+  while (queue.length) {
+    const node = queue.shift();
+    visited.push(node.val);
+
+    if (node.left) {
+      queue.push(node.left);
+    }
+
+    if (node.right) {
+      queue.push(node.right);
+    }
+  }
+
+  return visited;
 }
 
 module.exports = {
-    breadthFirstArray
+  breadthFirstArray,
 };
