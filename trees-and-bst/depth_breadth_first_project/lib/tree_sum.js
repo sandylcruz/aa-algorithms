ccbbaefb34538c31bce1501c548419a5bf0c8fd6
Sandy Cruz <sandyleahcruz@icloud.com>
2021-06-27 11:46:55 -0700

    Get tree sum

--- trees-and-bst/depth_breadth_first_project/lib/tree_sum.js
+++ trees-and-bst/depth_breadth_first_project/lib/tree_sum.js
@@ -1,8 +1,25 @@
 function treeSum(root) {
+  let sum = 0;
+  const stack = [root];
 
+  if (!root) return 0;
+
+  while (stack.length) {
+    let node = stack.pop();
+
+    sum += node.val;
+
+    if (node.right) {
+      stack.push(node.right);
+    }
+
+    if (node.left) {
+      stack.push(node.left);
+    }
+  }
+  return sum;
 }
 
-
 module.exports = {
-    treeSum
+  treeSum,
 };
