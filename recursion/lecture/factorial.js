27cffda72e19eaa1624a971f08442bdb2f1d08a8
Sandy Cruz <sandyleahcruz@icloud.com>
2021-07-19 16:25:05 -0700

    Find nth fibonacci number recursively

--- recursion/lecture/factorial.js
+++ recursion/lecture/factorial.js
@@ -22,4 +22,13 @@ const factorial = (num) => {
   return num * factorial(num - 1);
 };
 
-console.log(factorial(5)); // => 120
+// console.log(factorial(5)); // => 120
+
+const fib = (num) => {
+  if (num === 1 || num === 2) {
+    return 1;
+  }
+
+  return fib(num - 1) + fib(num - 2);
+};
+console.log(fib(6));
