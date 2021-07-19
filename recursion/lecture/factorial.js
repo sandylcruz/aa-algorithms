260c297a66b0d4e291bd2043314b1ca6e4e1d1fe
Sandy Cruz <sandyleahcruz@icloud.com>
2021-07-19 13:55:40 -0700

    Return factorial of num recursively

---
+++ recursion/lecture/factorial.js
@@ -0,0 +1,25 @@
+// Write a method `factorial(n)` which takes a number and returns the factorial of n.
+// A factorial is the product of all whole numbers between 1 and n, inclusive.
+// For example, `factorial(5)` is 5 * 4 * 3 * 2 * 1 = 120.
+
+// factorial(5) = 5 * 4 * 3 * 2 * 1
+// factorial(4) = 4 * 3 * 2 * 1
+// factorial(3) = 3 * 2 * 1
+// factorial(2) = 2 * 1
+// factorial(1) = 1 (base case)
+
+// factorial(5) = 5 * factorial(4)
+// factorial(4) = 4 * factorial(3)
+// factorial(3) = 3 * factorial(2)
+// factorial(2) = 2 * factorial(1)
+// factorial(1) = 1 (base case)
+
+const factorial = (num) => {
+  if (num === 1) {
+    return 1;
+  }
+
+  return num * factorial(num - 1);
+};
+
+console.log(factorial(5)); // => 120
