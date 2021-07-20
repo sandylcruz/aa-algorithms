bd49741da7d049b9ab2c129fcc9c75799a6c9722
Sandy Cruz <sandyleahcruz@icloud.com>
2021-07-19 18:18:48 -0700

    Reverse string

--- recursion/recursion_project/lib/problems.js
+++ recursion/recursion_project/lib/problems.js
@@ -59,7 +59,13 @@ function sumArray(array) {
 // reverseString("c")           // => "c"
 // reverseString("internet")    // => "tenretni"
 // reverseString("friends")     // => "sdneirf"
-function reverseString(str) {}
+function reverseString(str) {
+  if (str.length === 0) {
+    return '';
+  }
+
+  return reverseString(str.slice(1)) + str[0];
+}
 
 // Write a function, pow(base, exponent), that takes in two numbers.
 // The function should calculate the base raised to the exponent power.
