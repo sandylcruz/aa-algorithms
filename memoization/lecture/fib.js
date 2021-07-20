f7f1b44af463b64776e37d019782ce6e97005b97
Sandy Cruz <sandyleahcruz@icloud.com>
2021-07-20 15:06:18 -0700

    Write fibonacci with memoization

---
+++ memoization/lecture/fib.js
@@ -0,0 +1,17 @@
+const fib = (num, memo = {}) => {
+  if (num === 1 || num === 0) {
+    return 1;
+  }
+
+  if (num in memo) {
+    console.log(`fetching ${num}`);
+    return memo[num];
+  }
+
+  const result = fib(num - 1, memo) + fib(num - 2, memo);
+  memo[num] = result;
+  console.log(`stored result in memo for num: ${num}`);
+  return result;
+};
+console.log(fib(5));
+console.log(fib(100));
