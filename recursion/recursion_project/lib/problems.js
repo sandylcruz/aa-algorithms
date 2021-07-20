0683a25f43efea7c38a369ce57931aeaa77eafc3
Sandy Cruz <sandyleahcruz@icloud.com>
2021-07-19 18:29:43 -0700

    Return base raised to exponent power recursively

--- recursion/recursion_project/lib/problems.js
+++ recursion/recursion_project/lib/problems.js
@@ -83,7 +83,19 @@ function reverseString(str) {
 // pow(2, 5)    // => 32
 // pow(3, 4)    // => 81
 // pow(2, -5)   // => 0.03125
-function pow(base, exponent) {}
+function pow(base, exponent) {
+  if (exponent === 0) {
+    return 1;
+  } else if (exponent === 1) {
+    return base;
+  }
+
+  if (exponent < 0) {
+    return 1 / pow(base, -exponent);
+  }
+
+  return base * pow(base, exponent - 1);
+}
 
 // A 1-dimensional array is also known as a flattened array.
 // Write a method, flatten(data), that accepts a single argument. The
