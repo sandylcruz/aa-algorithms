127ad3622d4d4da1d93b7fbd498e2e2b7340a0a5
Sandy Cruz <sandyleahcruz@icloud.com>
2021-07-21 08:45:30 -0700

    Return minimum number of coins needed to make target amount

--- memoization/memoization_project/lib/leet_code_518.js
+++ memoization/memoization_project/lib/leet_code_518.js
@@ -1,3 +1,14 @@
 // Work through this problem on https://leetcode.com/problems/coin-change-2/ and use the specs given there.
 // Feel free to use this file for scratch work.
 
+/*
+You are given an integer array coins representing coins of different denominations and an integer 
+amount representing a total amount of money.
+
+Return the number of combinations that make up that amount. If that amount of money cannot be made up by any 
+combination of the coins, return 0.
+
+You may assume that you have an infinite number of each kind of coin.
+
+The answer is guaranteed to fit into a signed 32-bit integer.
+*/
--- memoization/memoization_project/lib/problems.js
+++ memoization/memoization_project/lib/problems.js
@@ -29,7 +29,7 @@ function lucasNumberMemo(n, memo = {}) {
 }
 
 // Write a function, minChange(coins, amount), that accepts an array of coin values
-// and a target amount as arguments. The method should the minimum number of coins needed
+// and a target amount as arguments. The method should return the minimum number of coins needed
 // to make the target amount. A coin value can be used multiple times.
 //
 // After you pass the first 3 examples, you'll likely need to memoize your code
@@ -41,7 +41,24 @@ function lucasNumberMemo(n, memo = {}) {
 // minChange([1, 4, 5], 8))         // => 2, because 4 + 4 = 8
 // minChange([1, 5, 10, 25], 15)    // => 2, because 10 + 5 = 15
 // minChange([1, 5, 10, 25], 100)   // => 4, because 25 + 25 + 25 + 25 = 100
-function minChange(coins, amount, memo = {}) {}
+function minChange(coins, amount, memo = {}) {
+  if (amount in memo) {
+    return memo[amount];
+  }
+
+  if (amount === 0) return 0;
+
+  let numCoins = [];
+  coins.forEach((coin) => {
+    if (coin <= amount) {
+      numCoins.push(minChange(coins, amount - coin, memo) + 1);
+    }
+  });
+
+  memo[amount] = Math.min(...numCoins);
+  return memo[amount];
+}
+console.log(minChange([1, 2, 5], 11, (memo = {})));
 
 module.exports = {
   lucasNumberMemo,
