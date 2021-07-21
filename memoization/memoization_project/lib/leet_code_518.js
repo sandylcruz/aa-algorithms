6a18bcde6ebc530ff2c2e04d63ea2a9d16c4ff91
Sandy Cruz <sandyleahcruz@icloud.com>
2021-07-21 11:33:43 -0700

    Return count of number of unique combinations of coins to make amount

--- memoization/memoization_project/lib/leet_code_518.js
+++ memoization/memoization_project/lib/leet_code_518.js
@@ -12,3 +12,21 @@ You may assume that you have an infinite number of each kind of coin.
 
 The answer is guaranteed to fit into a signed 32-bit integer.
 */
+
+const change = (amount, coins, memo = {}) => {
+  let key = amount + '-' + coins;
+  if (key in memo) return memo[key];
+  if (amount === 0) return 1;
+
+  let currentCoin = coins[coins.length - 1];
+
+  let total = 0;
+
+  for (let qty = 0; qty * currentCoin <= amount; qty++) {
+    total += change(amount - qty * currentCoin, coins.slice(0, -1), memo);
+  }
+  memo[key] = total;
+  return memo[key];
+};
+
+console.log(change(5, [1, 2, 5]));
