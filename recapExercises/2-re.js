764301d556cac1acbeb1ea73f7f54f53f6c35f25
Sandy Cruz <sandyleahcruz@icloud.com>
2021-07-28 11:07:46 -0700

    Write fizzbuzz

--- recapExercises/2-re.js
+++ recapExercises/2-re.js
@@ -62,12 +62,19 @@ function most_frequent_bigram(string) {
 # Write a method, #inverse, that returns a new hash where the key-value pairs are swapped
 */
 function inverse(object) {
-  const newObject = {};
+  const inversedObject = {};
+  const previousKeys = Object.keys(object);
+  const previousValues = Object.values(object);
+  console.log(previousKeys, previousValues);
 
-  return newObject;
+  const previousObject = Object.entries(object);
+  console.log(previousObject);
+
+  return inversedObject;
 }
 
 // console.log(inverse({ a: 1, b: 2 })); // => {1: 'a', 2: 'b'}
+// console.log(inverse({ a: 1, b: 1 })); // => {1: ['a', 'b']}
 // console.log(inverse({ name: 'fido', hungry: true })); // => {"fido": "name", true: 'hungry'}
 
 /*
@@ -88,9 +95,9 @@ function pairSumCount(array, targetNum) {
 
   return counter;
 }
-console.log(pairSumCount([1, 2, 1, 3, 8], 5)); // => 1
-console.log(pairSumCount([10, 3, 6, 5, 7], 13)); // => 2
-console.log(pairSumCount([10, 3, 6, 5, 7], 4)); // => 0
+// console.log(pairSumCount([1, 2, 1, 3, 8], 5)); // => 1
+// console.log(pairSumCount([10, 3, 6, 5, 7], 13)); // => 2
+// console.log(pairSumCount([10, 3, 6, 5, 7], 4)); // => 0
 
 /*
 # Write a method, Array#bubble_sort, that takes in an optional proc argument.
@@ -111,3 +118,24 @@ console.log(pairSumCount([10, 3, 6, 5, 7], 4)); // => 0
 function bubbleSort() {}
 // console.log(bubbleSort([4, 12, 2, 8, 1, 14, 9, 25, 24, 81])); // => [1, 2, 4, 8, 9, 12, 14, 24, 25, 81]
 // console.log(bubbleSort([5, 4, 7])); // => [4, 5, 7]
+
+const fizzBuzz = (num) => {
+  const arrayToReturn = [];
+
+  for (let i = 1; i <= num; i++) {
+    if (i % 15 === 0) {
+      arrayToReturn.push('Fizzbuzz');
+    } else if (i % 3 === 0) {
+      arrayToReturn.push('Fizz');
+    } else if (i % 5 === 0) {
+      arrayToReturn.push('Buzz');
+    } else {
+      arrayToReturn.push(i);
+    }
+  }
+
+  return arrayToReturn;
+};
+
+console.log(fizzBuzz(35)); // =>
+// [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'Fizz Buzz', 16, 17, 'Fizz', 19, 'Buzz', 'Fizz', 22, 23, 'Fizz', 'Buzz', 26, 'Fizz', 28, 29, 'Fizz Buzz', 31, 32, 'Fizz', 34, 'Buzz', 'Fizz']
