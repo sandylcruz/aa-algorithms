4918b9e5033e754da7a6592e153a76bbc86686b1
Sandy Cruz <sandyleahcruz@icloud.com>
2021-08-01 14:55:42 -0700

    Solve leetcode 16)

--- tabulation/lecture/lecture.js
+++ tabulation/lecture/lecture.js
@@ -1,15 +1,15 @@
 // tabulated solution
-
 const tabulatedFib = (num) => {
   let table = new Array(num + 1);
   table[0] = 0;
   table[1] = 1;
 
+  // complete the table by moving from left to right,
+  // following the fibonacci pattern
   for (let i = 2; i < table.length; i++) {
     table[i] = table[i - 1] + table[i - 2];
   }
   console.log(table);
-
   return table[num];
 };
 
@@ -44,19 +44,39 @@ the segmentation.
 */
 
 const wordBreak = (string, dictionary) => {
-  // table size should be equal to string length
-  let table = new Array(string.length + 1);
+  // table size should be equal to string length + 1 because it's zero based
+  // initialize table to contain all falses at start
+  let table = new Array(string.length + 1).fill(false);
   // empty string is recognized. table[0] is always true
   table[0] = true;
 
   for (let i = 0; i < table.length; i++) {
     // iterate through potential starting points for our next word; consider only if entry on table is true;
     // if table at this position is a starting point, continue
+
+    // iterate through possible starting points
+    // if table says we can't start here, skip to next position
+    // if false, don't use at starting point because there's no legal way to break up the word
+
     if (table[i] === false) continue;
 
-    for (let j = i + 1)
+    // if table[i] is true, then iterate potential ending points
+    // ending point must be after starter
+    for (let j = i + 1; j < table.length; j++) {
+      // start at start index, and end before end index j
+      let word = string.slice(i, j);
+      if (dictionary.includes(word)) {
+        table[j] = true;
+      }
+    }
   }
+
+  // console.log(table);
+  return table[table.length - 1];
 };
 
 console.log(wordBreak('leetcode', ['leet', 'code'])); // => true
-console.log(wordBreak('applepen', ['apple', 'pen'])); // => true
+console.log(wordBreak('applepen', ['app', 'apple', 'pen'])); // => true
+console.log(wordBreak('catsandog', ['cats', 'dog', 'sand', 'and', 'cat'])); // => false
+
+// time complexity: O(n^2)
