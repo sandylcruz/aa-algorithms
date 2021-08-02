282aea330582f58c4119ca4d1edfc1af69b678e0
Sandy Cruz <sandyleahcruz@icloud.com>
2021-08-01 21:16:47 -0700

    Write swap

---
+++ naive-sorting-algorithms/selection-sort/selection_sort_project/lib/selection_sort.js
@@ -0,0 +1,13 @@
+function swap(arr, index1, index2) {
+  const temp = arr[index1];
+  arr[index1] = arr[index2];
+  arr[index2] = temp;
+  return arr;
+}
+
+function selectionSort(arr) {}
+
+module.exports = {
+  selectionSort,
+  swap,
+};
