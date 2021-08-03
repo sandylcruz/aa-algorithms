20e1244cb24ce42e4876da2bfa500ada810e122a
Sandy Cruz <sandyleahcruz@icloud.com>
2021-08-02 19:37:38 -0700

    Solve selection sort with nested loop

--- naive-sorting-algorithms/selection-sort/selection_sort_project/lib/selection_sort.js
+++ naive-sorting-algorithms/selection-sort/selection_sort_project/lib/selection_sort.js
@@ -5,7 +5,23 @@ function swap(arr, index1, index2) {
   return arr;
 }
 
-function selectionSort(arr) {}
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[minIndex] > arr[j]) {
+        minIndex = j;
+      }
+    }
+
+    swap(arr, i, minIndex);
+  }
+
+  return arr;
+}
+
+console.log(selectionSort([5, 4, 3, 2, 1]));
 
 module.exports = {
   selectionSort,
