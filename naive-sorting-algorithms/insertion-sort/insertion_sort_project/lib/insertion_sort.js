41bb1be38f547c9f4342c687836889b3106c5c7c
Sandy Cruz <sandyleahcruz@icloud.com>
2021-08-05 12:02:43 -0700

    Write insertion sort

--- naive-sorting-algorithms/insertion-sort/insertion_sort_project/lib/insertion_sort.js
+++ naive-sorting-algorithms/insertion-sort/insertion_sort_project/lib/insertion_sort.js
@@ -1,7 +1,51 @@
-function insertionSort(arr) {
+// function insertionSort(arr) {
+//   for (let i = 1; i < arr.length; i++) {
+//     let currentNum = arr[i];
+//     let j = i - 1;
 
-}
+//     while (j >= 0 && arr[j] > currentNum) {
+//       arr[j + 1] = arr[j];
+//       j--;
+//     }
 
+//     arr[j + 1] = currentNum;
+//   }
+
+//   return arr;
+// }
+
+// const insertionSort = (arr) => {
+//   for (let i = 1; i < arr.length; i++) {
+//     let currentNum = arr[i];
+//     let j = i - 1;
+
+//     while (j >= 0 && arr[j] > currentNum) {
+//       arr[j + 1] = arr[j];
+//       j--;
+//     }
+
+//     arr[j + 1] = currentNum;
+//   }
+
+//   return arr;
+// };
+
+const insertionSort = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    let currentNum = arr[i];
+    let j = i - 1;
+
+    while (j >= 0 && arr[j] > currentNum) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+
+    arr[j + 1] = currentNum;
+  }
+  return arr;
+};
+
+console.log(insertionSort([5, 4, 3, 2, 1]));
 module.exports = {
-    insertionSort
+  insertionSort,
 };
