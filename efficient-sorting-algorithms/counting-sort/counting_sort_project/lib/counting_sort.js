415bed68d1b47197dc6219413c5a0e31a98ef2e3
Sandy Cruz <sandyleahcruz@icloud.com>
2021-08-09 16:53:22 -0700

    Write counting sort with different iteration

--- efficient-sorting-algorithms/counting-sort/counting_sort_project/lib/counting_sort.js
+++ efficient-sorting-algorithms/counting-sort/counting_sort_project/lib/counting_sort.js
@@ -1,8 +1,58 @@
-function countingSort(arr, max) {
+function countingSort1(arr, max) {
+  const counter = new Array(max + 1).fill(0);
+  const result = [];
 
+  arr.forEach((num) => {
+    counter[num] += 1;
+  });
+
+  for (let i = 0; i < counter.length; i++) {
+    while (counter[i] > 0) {
+      result.push(i);
+      counter[i]--;
+    }
+  }
+
+  return result;
 }
 
+const countingSort2 = (arr, max) => {
+  const counter = new Array(max + 1).fill(0);
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    counter[arr[i]]++;
+  }
+
+  for (let j = 0; j < counter.length; j++) {
+    while (counter[j] > 0) {
+      result.push(j);
+      counter[j]--;
+    }
+  }
+  return result;
+};
+
+const countingSort = (arr, max) => {
+  const counter = new Array(max + 1).fill(0);
+  const result = [];
+
+  arr.forEach((num) => {
+    counter[num] += 1;
+  });
+
+  for (let i = 0; i < counter.length; i++) {
+    while (counter[i] > 0) {
+      result.push(i);
+      counter[i]--;
+    }
+  }
+
+  return result;
+};
+
+console.log(countingSort([5, 5, 5, 4, 3, 2, 1], 5));
 
 module.exports = {
-    countingSort
+  countingSort,
 };
