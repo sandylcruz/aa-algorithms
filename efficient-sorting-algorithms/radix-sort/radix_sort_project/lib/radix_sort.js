c5361082f5a625f5373a16158d815ddeabb0a8d7
Sandy Cruz <sandyleahcruz@icloud.com>
2021-08-09 16:31:02 -0700

    Write radix sort

--- efficient-sorting-algorithms/radix-sort/radix_sort_project/lib/radix_sort.js
+++ efficient-sorting-algorithms/radix-sort/radix_sort_project/lib/radix_sort.js
@@ -1,22 +1,16 @@
 const getDigitFrom = (num, place) =>
   Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
 
-const getIntLength = (num) => {
-  if (num === 0) {
-    return 1;
-  } else {
-    Math.floor(Math.log10(Math.abs(num))) + 1;
-  }
-};
+const getIntLength = (num) =>
+  num === 0 ? 1 : Math.floor(Math.log10(Math.abs(num))) + 1;
 
-const getMaxDigits = (nums) => {
+function getMaxDigits(nums) {
   let maxDigits = 0;
   for (let i = 0; i < nums.length; i++) {
     maxDigits = Math.max(maxDigits, getIntLength(nums[i]));
   }
-
   return maxDigits;
-};
+}
 
 function radixSortNonNegative(arr) {
   if (!Array.isArray(arr)) return null;
@@ -37,6 +31,25 @@ function radixSortNonNegative(arr) {
     arr = [].concat(...buckets);
   }
 
+  return arr;
+}
+
+function radixSort(arr) {
+  if (!Array.isArray(arr)) {
+    return null;
+  }
+
+  let maxDigits = getMaxDigits(arr);
+  for (let k = 0; k < maxDigits; k++) {
+    let buckets = Array.from({ length: 10 }, () => []); // Array of empty arrays
+
+    for (let i = 0; i < arr.length; i++) {
+      let digit = getDigitFrom(arr[i], k);
+      buckets[digit].push(arr[i]);
+    }
+
+    arr = [].concat(...buckets);
+  }
   return arr;
 }
 
