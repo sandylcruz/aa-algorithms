ce7005f369bdaab8c680a05c3d3f497e903e81c0
Sandy Cruz <sandyleahcruz@icloud.com>
2021-08-09 16:23:44 -0700

    Write methods to get digits from num, get integer length, and get max digits

--- efficient-sorting-algorithms/radix-sort/radix_sort_project/lib/radix_sort.js
+++ efficient-sorting-algorithms/radix-sort/radix_sort_project/lib/radix_sort.js
@@ -1,5 +1,28 @@
+const getDigitFrom = (num, place) =>
+  Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
+
+const getIntLength = (num) => {
+  if (num === 0) {
+    return 1;
+  } else {
+    Math.floor(Math.log10(Math.abs(num))) + 1;
+  }
+};
+
+const getMaxDigits = (nums) => {
+  let maxDigits = 0;
+  for (let i = 0; i < nums.length; i++) {
+    maxDigits = Math.max(maxDigits, getIntLength(nums[i]));
+  }
+
+  return maxDigits;
+};
+
 function radixSort(arr) {
   if (!Array.isArray(arr)) return null;
+
+  if (arr.length === 0) return [];
+  if (arr.length === 1) return arr;
 }
 
 module.exports = {
