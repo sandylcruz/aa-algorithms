0c6c21b37864ec61f4f52eef20bdc57cecc459cd
Sandy Cruz <sandyleahcruz@icloud.com>
2021-08-09 18:51:55 -0700

    Write binary search to return boolean if target is in array

--- binary-search/binary_search_project/lib/binary_search.js
+++ binary-search/binary_search_project/lib/binary_search.js
@@ -1,13 +1,27 @@
 function binarySearch(array, target) {
+  if (array.length === 0) return false;
 
+  const middleIndex = Math.floor(array.length / 2);
+  const middleElement = array[middleIndex];
+  const left = array.slice(0, middleIndex);
+  const right = array.slice(middleIndex + 1);
+
+  if (target < middleElement) {
+    return binarySearch(left, target);
+  } else if (target > middleElement) {
+    return binarySearch(right, target);
+  } else {
+    return true;
+  }
 }
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10));
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7], 7));
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7], 8));
+console.log(binarySearch([-1, 2, 3, 4, 5, 6, -7], 7));
 
-function binarySearchIndex(array, target) {
-
-}
-
+function binarySearchIndex(array, target) {}
 
 module.exports = {
-    binarySearch,
-    binarySearchIndex
+  binarySearch,
+  binarySearchIndex,
 };
