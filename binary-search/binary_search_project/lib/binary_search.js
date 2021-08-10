f4e30a6309fd95894217159f3941aa1473fa8e1c
Sandy Cruz <sandyleahcruz@icloud.com>
2021-08-10 10:31:53 -0700

    Return index of target item using binary search

--- binary-search/binary_search_project/lib/binary_search.js
+++ binary-search/binary_search_project/lib/binary_search.js
@@ -14,12 +14,35 @@ function binarySearch(array, target) {
     return true;
   }
 }
-console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10));
-console.log(binarySearch([1, 2, 3, 4, 5, 6, 7], 7));
-console.log(binarySearch([1, 2, 3, 4, 5, 6, 7], 8));
-console.log(binarySearch([-1, 2, 3, 4, 5, 6, -7], 7));
+// console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10));
+// console.log(binarySearch([1, 2, 3, 4, 5, 6, 7], 7));
+// console.log(binarySearch([1, 2, 3, 4, 5, 6, 7], 8));
+// console.log(binarySearch([-1, 2, 3, 4, 5, 6, -7], 7));
 
-function binarySearchIndex(array, target) {}
+function binarySearchIndex(array, target) {
+  let low = 0;
+  let high = array.length - 1;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+
+    if (target < array[mid]) {
+      high = mid - 1;
+    } else if (target > array[mid]) {
+      low = mid + 1;
+    } else {
+      return mid;
+    }
+  }
+
+  return -1;
+}
+
+console.log(binarySearchIndex([5, 10, 12, 15, 20, 30, 70], 12)); // 2
+console.log(binarySearchIndex([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10)); // 9
+console.log(binarySearchIndex([1, 2, 3, 4, 5, 6, 7], 7)); // 6
+console.log(binarySearchIndex([1, 2, 3, 4, 5, 6, 7], 8)); // -1
+console.log(binarySearchIndex([-1, 2, 3, 4, 5, 6, -7], 7)); // -1
 
 module.exports = {
   binarySearch,
