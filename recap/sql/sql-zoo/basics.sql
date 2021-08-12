82f8a191779d15808ee20012a1f068d3edabdd80
Sandy Cruz <sandyleahcruz@icloud.com>
2021-08-11 21:09:40 -0700

    Format SQL uniformly

--- recap/sql/sql-zoo/basics.sql
+++ recap/sql/sql-zoo/basics.sql
@@ -1,9 +1,12 @@
-SELECT population FROM world
-  WHERE name = 'Germany'
+SELECT population 
+FROM world
+WHERE name = 'Germany'
 
-SELECT name, population FROM world
-  WHERE name IN ('Sweden', 'Norway', 'Denmark');
+SELECT name, population 
+FROM world
+WHERE name IN ('Sweden', 'Norway', 'Denmark');
 
-
-SELECT name, area FROM world
-  WHERE area BETWEEN 200000 AND 250000
+SELECT name, area 
+FROM world
+WHERE area 
+BETWEEN 200000 AND 250000
