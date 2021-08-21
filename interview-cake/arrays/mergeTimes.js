62ae314e9a71339a6a42e01af10ef0cf6a51585a
Sandy Cruz <sandyleahcruz@icloud.com>
2021-08-20 18:16:55 -0700

    Merge meeting times if overlapping

---
+++ interview-cake/arrays/mergeTimes.js
@@ -0,0 +1,35 @@
+/*
+Write a function mergeRanges() that takes an array of multiple meeting time ranges and returns an array of condensed ranges.
+*/
+
+const meetingMerger = (array) => {
+  const sortedMeetings = array.sort((a, b) => a.startTime - b.startTime);
+
+  const finalMeetings = [sortedMeetings[0]];
+
+  for (let i = 1; i < sortedMeetings.length; i++) {
+    const currentMeeting = sortedMeetings[i];
+    const nextMeeting = finalMeetings[finalMeetings.length - 1];
+
+    if (currentMeeting.startTime <= nextMeeting.endTime) {
+      nextMeeting.endTime = Math.max(
+        currentMeeting.endTime,
+        nextMeeting.endTime
+      );
+    } else {
+      finalMeetings.push(currentMeeting);
+    }
+  }
+
+  return finalMeetings;
+};
+
+const times = [
+  { startTime: 0, endTime: 1 },
+  { startTime: 3, endTime: 5 },
+  { startTime: 4, endTime: 8 },
+  { startTime: 10, endTime: 12 },
+  { startTime: 9, endTime: 10 },
+];
+
+console.log(meetingMerger(times)); // =>   [{ startTime: 0, endTime: 1 }, { startTime: 3, endTime: 8 }, { startTime: 9, endTime: 12 }]
