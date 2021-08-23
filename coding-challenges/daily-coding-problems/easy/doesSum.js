4d5c36f2de3a8fd38d224e5390e1b9e4d38a2db5
Sandy Cruz <sandyleahcruz@icloud.com>
2021-08-22 22:28:27 -0700

    Return whether any two numbers from list add up to target sum

--- coding-challenges/daily-coding-problems/easy/doesSum.js
+++ coding-challenges/daily-coding-problems/easy/doesSum.js
@@ -4,6 +4,17 @@ Given a list of numbers and a number k, return whether any two numbers from the 
 For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17
 */
 
-const doesSum = (array, target) => {};
+const doesSum = (array, target) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
 
 console.log(doesSum([10, 15, 3, 7], 17));
+console.log(doesSum([10, 15, 3, 7], 25));
+console.log(doesSum([10, 15, 3, 7], 1));
