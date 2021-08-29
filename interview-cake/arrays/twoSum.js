61d2aa0694b396c4d752e7db256c20e4e4e4ae05
Sandy Cruz <sandyleahcruz@icloud.com>
2021-08-28 17:54:27 -0700

    Write two sum in linear time and space

--- interview-cake/arrays/twoSum.js
+++ interview-cake/arrays/twoSum.js
@@ -1,5 +1,5 @@
-// O(n^2) time
-const twoSum = (array, target) => {
+// O(n^2) time, O(1) space
+const quadraticTwoSum = (array, target) => {
   for (let i = 0; i < array.length; i++) {
     for (let j = i + 1; j < array.length; j++) {
       if (array[i] + array[j] === target) {
@@ -11,6 +11,21 @@ const twoSum = (array, target) => {
   return false;
 };
 
-console.log(twoSum([1, 2, 3, 4, 5], 7));
-console.log(twoSum([100, 21, 30, 4, 500], 104));
-console.log(twoSum([100, 200, 300, 400, 500], 104));
+// O(n) time, O(n) space
+const twoSum = (array, target) => {
+  const seenNumbers = new Set();
+
+  for (let i = 0; i < array.length; i++) {
+    const currentNumber = array[i];
+    const remainder = target - currentNumber;
+    if (seenNumbers.has(remainder)) {
+      return true;
+    } else {
+      seenNumbers.add(currentNumber);
+    }
+  }
+  return false;
+};
+console.log(twoSum([1, 2, 3, 4, 5], 7)); // true
+console.log(twoSum([100, 21, 30, 4, 500], 104)); // true
+console.log(twoSum([100, 200, 300, 400, 500], 104)); // false
