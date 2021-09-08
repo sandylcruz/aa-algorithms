10707cc2cd319c23252c80cecdaaf9262702e18b
Sandy Cruz <sandyleahcruz@icloud.com>
2021-09-07 19:59:06 -0700

    Convert file to typescript

--- coding-challenges/daily-coding-problems/easy/longestSubarray.ts
+++ coding-challenges/daily-coding-problems/easy/longestSubarray.ts
@@ -4,8 +4,8 @@ Given an array of elements, return the length of the longest subarray where all 
 For example, given the array [5, 1, 3, 5, 2, 3, 4, 1], return 5 as the longest subarray of distinct elements is [5, 2, 3, 4, 1].
 */
 
-const longestSubArray = (array) => {
-  let longestSubArray = [];
+const longestSubArray = (array: Array<number>) => {
+  let longestSubArray: Array<number> = [];
   let seenCharacters = new Set();
   let currentSubarray = [];
 
