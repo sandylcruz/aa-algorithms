dd59cb501d1a3989b6c1a66bf5b419545b7e09ee
Sandy Cruz <sandyleahcruz@icloud.com>
2021-09-20 18:44:02 -0700

    Return node value at given index, find target in linked list and return index

---
+++ structy/11-20/getNodeValue.js
@@ -0,0 +1,23 @@
+/*
+Write a function, getNodeValue, that takes in the head of a linked list and an 
+index. The function should return the value of the linked list at the specified 
+index.
+
+If there is no node at the given index, then return null.
+*/
+
+const getNodeValue = (head, index) => {
+  let count = 0;
+  let current = head;
+
+  while (current !== null) {
+    if (count === index) {
+      return current.val;
+    }
+
+    current = current.next;
+    count += 1;
+  }
+
+  return null;
+};
---
+++ structy/11-20/linkedListFind.js
@@ -0,0 +1,19 @@
+/*
+Write a function, linkedListFind, that takes in the head of a linked list 
+and a target value. The function should return a boolean indicating whether 
+or not the linked list contains the target.
+*/
+
+const linkedListFind = (head, target) => {
+  let current = head;
+
+  while (current !== null) {
+    if (current.val === target) {
+      return true;
+    }
+
+    current = current.next;
+  }
+
+  return false;
+};
