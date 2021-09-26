6c24ea110aab19336db206e6e88df7eb3d86c225
Sandy Cruz <sandyleahcruz@icloud.com>
2021-09-26 10:09:43 -0700

    Add linked lists together

---
+++ structy/21-30/addLists.js
@@ -0,0 +1,18 @@
+const addLists = (head1, head2, carry = 0) => {
+  if (head1 === null && head2 === null && carry === 0) return null;
+
+  const val1 = head1 === null ? 0 : head1.val;
+  const val2 = head2 === null ? 0 : head2.val;
+
+  const sum = val1 + val2 + carry;
+  const nextCarry = sum > 9 ? 1 : 0;
+  const digit = sum % 10;
+  const result = new Node(digit);
+
+  const next1 = head1 === null ? null : head1.next;
+  const next2 = head2 === null ? null : head2.next;
+
+  result.next = addLists(next1, next2, nextCarry);
+
+  return result;
+};
